ca97e362c4635a42c36d9917d3bfc71e810838e9
AmShaegar <amshaegar@wrenchbox.de>
2015-03-03 00:08:02 +0100

    added simple message module

---
+++ src/main/java/net/leaguecom/atlas/module/SimpleMessageModule.java
@@ -0,0 +1,24 @@
+package net.leaguecom.atlas.module;
+
+import net.leaguecom.atlas.Atlas;
+
+import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+import org.pircbotx.output.OutputChannel;
+import org.pircbotx.output.OutputUser;
+
+public class SimpleMessageModule implements Module {
+	public void execute(String cmd, String txt, GenericMessageEvent<Atlas> event) {
+		Atlas bot = event.getBot();
+		
+		if(event instanceof MessageEvent<?>) {
+			OutputChannel out = ((MessageEvent) event).getChannel().send();
+		} else {
+			OutputUser out = event.getUser().send();
+		}
+	}
+
+	public String help(String cmd) {
+		return "Print this help text.";
+	}
+}
